0c97a3cdab1a06ce7e376d4981fe853da53a4c39
yubraj pokharel <yubraj.pokharel555@gmail.com>
2016-06-29 20:57:11 -0500

    configuring hibernate

--- src/main/java/com/yubraj/configuration/WebConfig.java
+++ src/main/java/com/yubraj/configuration/WebConfig.java
@@ -1,12 +1,24 @@
 package com.yubraj.configuration;
 
+
+import com.yubraj.model.User;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by yubraj on 6/28/16.
@@ -15,7 +27,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.yubraj")
+@EnableTransactionManagement
 public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Autowired
+    Environment environment;
 
     @Bean
     public InternalResourceViewResolver resolver(){
@@ -24,6 +40,47 @@ public class WebConfig extends WebMvcConfigurerAdapter{
         resolver.setSuffix(".jsp");
         return resolver;
     }
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/onlineShop");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+
+        return dataSource;
+    }
+
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.hbm2ddl.auto","create");
+        return properties;
+    }
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+        sessionBuilder.addProperties(getHibernateProperties());
+        sessionBuilder.scanPackages("com.yubraj.model");
+        return sessionBuilder.buildSessionFactory();
+    }
+
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(
+            SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+                sessionFactory);
+
+        return transactionManager;
+    }
+
+
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
--- src/main/java/com/yubraj/controller/HomeController.java
+++ src/main/java/com/yubraj/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.yubraj.controller;
 
+import com.yubraj.dao.UserDao;
+import com.yubraj.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by yubraj on 6/28/16.
@@ -10,8 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserDao userDao;
+
     @RequestMapping(value = "/")
     public String home(){
+        User user = new User();
+        user.setId(12345);
+        user.setUsername("Yubraj");
+        user.setPassword("asdfasdf");
+        user.setEmail("asdfasdf");
+
+        userDao.save(user);
+
+        List<User> userList = userDao.findAll();
         return "home";
     }
 
